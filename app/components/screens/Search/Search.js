a0f81c76ef73a7f426c17a55dde8a5b5ac91523a
Samiksha <samiksha.jagtap@neosofttech.com>
2019-04-04 20:24:56 +0530

    Add search screen

--- app/components/screens/Search/Search.js
+++ app/components/screens/Search/Search.js
@@ -1,26 +1,13 @@
 import React, { Component } from "react";
-import { Text, View } from 'react-native';
+import { Text, View } from "react-native";
 import styles from "./styles";
-import Header from '../../../components/Header/Header';
 
 export default class Search extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-          
-        }
-      }
-  
-    render() {
-        return(
-            <View style={styles.container}>
-            <Header
-                title={'Search'}
-                isDrawer={false}
-                back={() => {this.props.navigation.goBack(null)}}/>
-            <Text style={styles.welcomeTxt}>Search</Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcomeTxt}>Search screen under development</Text>
+      </View>
+    );
   }
+}
