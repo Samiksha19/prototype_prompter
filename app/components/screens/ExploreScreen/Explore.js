3a3868552f310350db41a5924a6cf1cdab227f56
Samiksha <samiksha.jagtap@neosofttech.com>
2019-04-08 20:34:47 +0530

    Check network connection and render screen depending on status

--- app/components/screens/ExploreScreen/Explore.js
+++ app/components/screens/ExploreScreen/Explore.js
@@ -1,17 +1,37 @@
 import React from "react";
-import { View, Text, StatusBar, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+  AsyncStorage,
+  NetInfo,
+  ImageBackground,
+  ToastAndroid
+} from "react-native";
+import Loader from "../../Loader/Loader";
 import Icon from "react-native-vector-icons/MaterialIcons";
-import styles from "../../Header/styles";
+import styles from "./styles";
 import * as colors from "../../../utils/colors";
 import callApi from "../../../lib/apicaller";
+import Offline from "../Offline/Offline";
 import { IndicatorViewPager, PagerDotIndicator } from "rn-viewpager";
+
+const end_point = "random";
+const method = "GET";
 
 class Explore extends React.Component {
   constructor() {
     super();
     this.state = {
       articles: [],
-      loader: true
+      loader: true,
+      saved_articles: [],
+      loading: false,
+      icon_favorite: "favorite-border",
+      icon_color: colors.gray,
+      isConnected: true
     };
   }
 
@@ -26,6 +46,7 @@ class Explore extends React.Component {
           size={27}
         />
       ),
+      headerBackTitle: "",
       headerRight: (
         <Icon
           style={styles.menuIcon}
@@ -38,69 +59,199 @@ class Explore extends React.Component {
     };
   };
 
-  async componentDidMount() {
-    let end_point = "random";
-    let method = "GET";
+  UNSAFE_componentWillMount() {
+    NetInfo.getConnectionInfo().then(connectionInfo => {
+      if (connectionInfo.type === "none") {
+        this.setState({ isConnected: false });
+      }
+    });
+  }
+
+  componentDidMount() {
+    NetInfo.isConnected.addEventListener(
+      "connectionChange",
+      this.handleConnectivityChange
+    );
+    NetInfo.getConnectionInfo().then(connectionInfo => {
+      if (connectionInfo.type === "none") {
+        this.setState({ isConnected: false });
+      }
+    });
+    this.handleApiCall();
+  }
+
+  async handleApiCall() {
     try {
       let response = await callApi(end_point, method);
       this.setState({
         articles: response,
-        loader: false
+        loading: false
       });
     } catch (err) {
       alert(err);
     }
   }
 
-  render() {
-    return (
-      <View style={{ flex: 1, backgroundColor: "#EDEDED" }}>
-        <StatusBar translucent={false} barStyle="light-content" />
-        <IndicatorViewPager
-          style={{ flex: 1 }}
-          indicator={this._renderDotIndicator()}
-        >
-          {this.state.articles.map(article => (
-            <View style={styles.mainCarouselStyle}>
-              <TouchableOpacity
-                onPress={() =>
-                  this.props.navigation.navigate("ArticleFeed", {
-                    param: article
-                  })
-                }
-                style={{
-                  backgroundColor: "white",
-                  flex: 1,
-                  paddingBottom: 10
-                }}
-              >
-                <Image
-                  resizeMode="stretch"
-                  style={styles.imageStyle}
-                  source={require("../../../images/genfb.jpg")}
-                />
-                <Text
-                  style={{
-                    marginLeft: 20,
-                    marginTop: 25,
-                    fontSize: 18,
-                    fontWeight: "bold"
-                  }}
-                >
-                  {article.title}
-                </Text>
-                <Text style={{ flex: 1, margin: 20, fontSize: 15 }}>
-                  {article.teaser}
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.absoluteIconStyle}>
-                <Icon name="autorenew" color={colors.purple} size={28} />
-              </TouchableOpacity>
-            </View>
-          ))}
-        </IndicatorViewPager>
-      </View>
+  //   async saveArticle(article) {
+  //     this.state.icon_favorite === "favorite"
+  //       ? this.setState(
+  //           {
+  //             icon_favorite: "favorite-border",
+  //             icon_color: colors.grey
+  //           },
+  //           async () => {
+  //             let saved_articles = await AsyncStorage.getItem("SAVED_ARTICLES");
+  //             if (saved_articles === null) {
+  //               await AsyncStorage.setItem(
+  //                 "SAVED_ARTICLES",
+  //                 JSON.stringify(article)
+  //               );
+  //             } else {
+  //               //await AsyncStorage.setItem
+  //             }
+  //           }
+  //         )
+  //       : this.setState(
+  //           {
+  //             icon_favorite: "favorite",
+  //             icon_color: colors.red
+  //           },
+  //           async () => {
+  //             let saved_articles = await AsyncStorage.getItem("SAVED_ARTICLES");
+  //             if (saved_articles === null) {
+  //               await AsyncStorage.setItem(
+  //                 "SAVED_ARTICLES",
+  //                 JSON.stringify(article)
+  //               );
+  //             }
+  //           }
+  //         );
+  //   }
+
+  async refreshArticle() {
+    this.setState({ loading: true });
+    try {
+      let response = await callApi(end_point, method);
+      this.setState({
+        articles: response,
+        loading: false
+      });
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  handleConnectivityChange = isConnected => {
+    NetInfo.getConnectionInfo().then(connectionInfo => {
+      if (connectionInfo.type === "none") {
+        this.setState({ isConnected: false });
+      } else {
+        this.setState({ isConnected: true });
+        this.handleApiCall();
+      }
+    });
+
+    NetInfo.isConnected.fetch(connectionStatus => {
+      if (connectionStatus === "none") {
+        this.setState({ isConnected: false });
+      } else {
+        this.setState({ isConnected: true });
+      }
+    });
+  };
+
+  retryApiCall() {
+    if (this.state.isConnected) {
+      this.handleApiCall();
+    } else {
+      ToastAndroid.showWithGravity(
+        "No Internet Connection!",
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    NetInfo.isConnected.removeEventListener(
+      "connectionChange",
+      this.handleConnectivityChange
     );
+  }
+
+  render() {
+    if (!this.state.isConnected) {
+      return (
+        <View style={{ flex: 1 }}>
+          <Offline retryApiCall={() => this.retryApiCall()} />
+        </View>
+      );
+    } else {
+      debugger;
+      return (
+        <View style={{ flex: 1, backgroundColor: colors.grey }}>
+          <StatusBar translucent={false} barStyle="light-content" />
+          <IndicatorViewPager
+            style={{ flex: 1 }}
+            indicator={this._renderDotIndicator()}
+          >
+            {this.state.articles !== undefined &&
+              this.state.articles.map(article => (
+                <View style={styles.mainCarouselStyle}>
+                  <TouchableOpacity
+                    activeOpacity={0.9}
+                    onPress={() =>
+                      this.props.navigation.navigate("ArticleFeed", {
+                        param: article
+                      })
+                    }
+                    style={{
+                      backgroundColor: "white",
+                      flex: 1,
+                      paddingBottom: 10
+                    }}
+                  >
+                    <Image
+                      style={styles.imageStyle}
+                      source={require("../../../images/genfb.jpg")}
+                    />
+                    <View style={styles.icon_image_view_style}>
+                      <Icon
+                        name={this.state.icon_favorite}
+                        color={this.state.icon_color}
+                        size={28}
+                        //onPress={() => this.saveArticle(article)}
+                      />
+                    </View>
+                    <Text
+                      style={{
+                        marginLeft: 20,
+                        marginTop: 25,
+                        fontSize: 18,
+                        fontWeight: "bold"
+                      }}
+                    >
+                      {article.title}
+                    </Text>
+                    <Text style={{ flex: 1, margin: 20, fontSize: 15 }}>
+                      {article.teaser}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.absoluteIconStyle}>
+                    <Icon
+                      name="autorenew"
+                      color={colors.purple}
+                      size={28}
+                      onPress={() => this.refreshArticle()}
+                    />
+                  </TouchableOpacity>
+                </View>
+              ))}
+          </IndicatorViewPager>
+          <Loader visible={this.state.loading} />
+        </View>
+      );
+    }
   }
 
   _renderDotIndicator() {
--- app/components/screens/ExploreScreen/styles.js
+++ app/components/screens/ExploreScreen/styles.js
@@ -1,6 +1,6 @@
 import { Platform, StyleSheet } from "react-native";
-import globals from "../../lib/globals";
-import * as colors from "../../utils/colors";
+import globals from "../../../lib/globals";
+import * as colors from "../../../utils/colors";
 
 export default (styles = StyleSheet.create({
   container: {
@@ -25,8 +25,20 @@ export default (styles = StyleSheet.create({
     color: colors.white
   },
   menuIcon: {
+    marginRight: 10,
     backgroundColor: colors.purple,
     marginLeft: 10
+  },
+  mainCarouselStyle: {
+    flex: 1,
+    backgroundColor: "#EDEDED",
+    paddingHorizontal: 20,
+    paddingBottom: 65,
+    paddingTop: 20
+  },
+  imageStyle: {
+    width: "100%",
+    height: 250
   },
   cardStyle: {
     flex: 1,
@@ -39,5 +51,68 @@ export default (styles = StyleSheet.create({
     alignItems: "stretch",
     backgroundColor: "#FFF",
     borderWidth: StyleSheet.hairlineWidth
+  },
+  absoluteIconStyle: {
+    flex: 1,
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    borderColor: colors.purple,
+    backgroundColor: colors.white,
+    alignSelf: "center",
+    borderWidth: 2,
+    position: "absolute",
+    bottom: 40,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  icon_borderStyle: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    shadowOffset: {
+      width: 10,
+      height: 10
+    },
+    shadowColor: colors.black,
+    shadowOpacity: 1.0
+  },
+  containerStyle: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  offlineImage: {
+    flex: 1
+  },
+  offline_imageStyle: { width: "90%", height: "50%" },
+  offline_textStyle: {
+    fontSize: 16,
+    color: colors.black,
+    textAlign: "center",
+    marginVertical: 20
+  },
+  refreshViewStyle: {
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    borderRadius: 4,
+    elevation: 1,
+    zIndex: 99,
+    backgroundColor: colors.purple
+  },
+  icon_image_view_style: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+    height: 40,
+    width: 40,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.white,
+    shadowOpacity: 100
   }
 }));
