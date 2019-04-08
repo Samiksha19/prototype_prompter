2ef03558a2fd8d399efc9475740790867facde6c
Samiksha <samiksha.jagtap@neosofttech.com>
2019-04-08 20:33:31 +0530

    add loader file

--- app/components/Loader/Loader.js
+++ app/components/Loader/Loader.js
@@ -1,23 +1,33 @@
-
 //loader that get called on page open
-import React from 'react';
-import { ActivityIndicator } from 'react-native';
+import React from "react";
+import { View, ActivityIndicator, Modal } from "react-native";
+import styles from "./styles";
+import * as colors from "../../utils/colors";
 //Pure, stateless component
-const Loader = (props) => {
-    if (props.visible == true) {
-        return (
-               <ActivityIndicator
-                    animating={true}
-                    style={{                   
-                    marginTop: '60%',
-                    zIndex: 10
-                }}
-                    size="large"/>
-        );
-    }
-    else{
-        return null;
-    }
-}
+const Loader = props => {
+  if (props.visible == true) {
+    return (
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={props.visible}
+        presentationStyle="overFullScreen"
+      >
+        <View
+          style={styles.viewStyle}
+          pointerEvents={props.visible ? "none" : "auto"}
+        >
+          <ActivityIndicator
+            animating={true}
+            size="large"
+            color={colors.white}
+          />
+        </View>
+      </Modal>
+    );
+  } else {
+    return null;
+  }
+};
 
 export default Loader;
