b199206c755153058709a00b175a289ef74d2ce8
Samiksha <samiksha.jagtap@neosofttech.com>
2019-04-11 12:57:59 +0530

    fix warning with importing netInfo from react native

--- android/app/src/main/java/com/prototype_prompter/MainApplication.java
+++ android/app/src/main/java/com/prototype_prompter/MainApplication.java
@@ -3,6 +3,7 @@ package com.prototype_prompter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new NetInfoPackage(),
             new RealmReactPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
