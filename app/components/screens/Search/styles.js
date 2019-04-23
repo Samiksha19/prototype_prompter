d07e64e00355dcad9f527aeca5cf27084ff3c924
Samiksha <samiksha.jagtap@neosofttech.com>
2019-04-23 11:42:22 +0530

    Created Search screen UI along with root stack header changes and redux setup for search screen

--- app/components/screens/Search/styles.js
+++ app/components/screens/Search/styles.js
@@ -1,14 +1,41 @@
-import { StyleSheet } from "react-native";
-import * as FontSizes  from '../../../utils/fontsSizes';
-import * as colors from '../../../utils/colors';
+import { StyleSheet, Platform } from "react-native";
+import * as FontSizes from "../../../utils/fontsSizes";
+import * as colors from "../../../utils/colors";
 
-export default styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    welcomeTxt: {
-        alignSelf: 'center',
-        fontSize: FontSizes.medium,
-        marginTop: 40
-    }
-});
+export default (styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  headerStyles: {
+    flexDirection: "row",
+    height: Platform.OS === "ios" ? 65 : 50,
+    backgroundColor: colors.purple,
+    justifyContent: "space-around",
+    alignItems: "flex-end"
+  },
+  welcomeTxt: {
+    alignSelf: "center",
+    fontSize: FontSizes.medium,
+    marginTop: 40
+  },
+  menuIcon: {
+    backgroundColor: colors.purple,
+    marginHorizontal: 10,
+    paddingBottom: 10
+  },
+  headerTextInputStyle: {
+    flex: 1,
+    color: colors.white,
+    fontSize: 16,
+    paddingLeft: 5
+  },
+  textInputViewStyle: {
+    flexDirection: "row",
+    flex: 2,
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderBottomWidth: 2,
+    marginBottom: 5,
+    borderBottomColor: colors.white
+  }
+}));
