13b47d039ad1c188f8f6b1bd0bc833cbdab2917f
Samiksha <samiksha.jagtap@neosofttech.com>
2019-04-29 20:17:44 +0530

    Improved UI of modal on search screen and UI modifications of search when screen is blank

--- app/components/screens/SearchHistory/SearchHistoryList.js
+++ app/components/screens/SearchHistory/SearchHistoryList.js
@@ -7,24 +7,45 @@ import styles from "./styles";
 class SearchHistoryList extends Component {
   render() {
     return (
-      <ScrollView>
-        <Text style={styles.searchTopTextStyle}>
-          {"Previously Searched Keywords"}
-        </Text>
-        {this.props.data.map((element, index) => (
-          <View key={index}>
-            <TouchableOpacity
-              onPress={() => this.props.onPress(element)}
-              activeOpacity={0.7}
-              style={styles.elementStyle}
-            >
-              <Text style={styles.listTextStyle}>{element}</Text>
-              <Icon name="history" size={27} color={colors.gray} />
-            </TouchableOpacity>
-            <View style={styles.viewBorderStyle} />
+      <View style={{ flex: 1 }}>
+        {this.props.data.length > 0 ? (
+          <ScrollView>
+            <Text style={styles.searchTopTextStyle}>
+              {"Previously Searched Keywords"}
+            </Text>
+            {this.props.data.map((element, index) => (
+              <View key={index}>
+                <TouchableOpacity
+                  onPress={() => this.props.onPress(element)}
+                  activeOpacity={0.7}
+                  style={styles.elementStyle}
+                >
+                  <Text style={styles.listTextStyle}>{element}</Text>
+                  <Icon name="history" size={27} color={colors.gray} />
+                </TouchableOpacity>
+                <View style={styles.viewBorderStyle} />
+              </View>
+            ))}
+          </ScrollView>
+        ) : (
+          <View style={styles.blankScreenStyle}>
+            <View style={styles.iconContainerStyle}>
+              <Icon
+                name="search"
+                size={50}
+                style={styles.largeIconStyle}
+                color={colors.gray}
+              />
+            </View>
+            <Text style={styles.blankScreenHeaderTextStyle}>
+              {"No Search Data Found!"}
+            </Text>
+            <Text style={styles.detailTextStyle}>
+              {"Begin your first search with any keyword!"}
+            </Text>
           </View>
-        ))}
-      </ScrollView>
+        )}
+      </View>
     );
   }
 }
--- app/components/screens/SearchHistory/styles.js
+++ app/components/screens/SearchHistory/styles.js
@@ -2,6 +2,10 @@ import { StyleSheet } from "react-native";
 import * as colors from "../../../utils/colors";
 
 export default (styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.grey
+  },
   elementStyle: {
     backgroundColor: colors.white,
     flex: 1,
@@ -28,5 +32,38 @@ export default (styles = StyleSheet.create({
     paddingLeft: 16,
     paddingVertical: 15,
     color: colors.black
+  },
+  blankScreenStyle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  largeIconStyle: {
+    marginVertical: 35,
+    position: "absolute"
+  },
+  blankScreenHeaderTextStyle: {
+    fontSize: 17,
+    fontWeight: "600",
+    color: colors.gray
+  },
+  detailTextStyle: {
+    fontSize: 16,
+    fontWeight: "100",
+    color: colors.gray,
+    paddingVertical: 16
+  },
+  iconContainerStyle: {
+    width: 80,
+    height: 80,
+    borderWidth: 2,
+    borderColor: colors.gray,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 40
+  },
+  searchHistoryListViewStyle: {
+    paddingVertical: 15
   }
 }));
